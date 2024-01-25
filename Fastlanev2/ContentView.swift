7f3a7abe47d467ca6a622b91eac872bd50597a3c
Ignacio Arias <1992ignacio@gmail.com>
2024-01-25 03:01:07 -0800

    initial commit

---
+++ Fastlanev2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fastlanev2
+//
+//  Created by Ignacio Arias on 2024-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world, Fastlanev2!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fastlanev2/Fastlanev2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fastlanev2App.swift
+//  Fastlanev2
+//
+//  Created by Ignacio Arias on 2024-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Fastlanev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
